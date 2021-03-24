1ee09262899fd0cb6894b6c901b31a50ff9d67dc
Serdar <serdar2225@gmail.com>
2021-03-23 23:48:17 -0400

    interface specifications

---
+++ src/main/java/AccessModifiers/Interfaces/Engine.java
@@ -0,0 +1,20 @@
+package AccessModifiers.Interfaces;
+
+public interface Engine {
+/*
+Interface:
+A reference type that can contain method signatures, default methods, static methods, nested types and constants.
+
+* Interfaces cannot be instantiated
+* A clas can implement one or more interfaces
+* An interface cannot contain a constructor
+* Usually  interdace methods do not have a body
+* Default and static methods have been introduced in recent versons of Java
+
+Why to use interfaces
+* abstraction : Hide away implementation details
+* Mimic multiple inheritance:
+
+ */
+
+}
