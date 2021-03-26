edb0780a6fc59c2576197f3ee6e61f426e2cbe70
Serdar <serdar2225@gmail.com>
2021-03-25 23:52:03 -0400

    abstract vs interface

--- src/main/java/AccessModifiers/Polymorphism/RadarInterface.java
+++ src/main/java/AccessModifiers/Polymorphism/RadarInterface.java
@@ -1,5 +1,24 @@
 package AccessModifiers.Polymorphism;
 
+/*
+Change behavior of an application at runtime even without recompligin your code
+
+REduces coupling because we can depend on abstractions not concrete types
+
+We can use a single variable type to store many types
+
+When to use interface vs abstract???
+
+abstract                                    Interface
+When you want to offer some base            Highest level of abstraction
+functionality to subclasses                 Highest level of lose coupling
+                                            Implement more interfaces
+Provide template for future classes
+
+Create abstract members that are not
+public
+ */
+
 public interface RadarInterface {
     List<RadarTarget> readData();
 }
