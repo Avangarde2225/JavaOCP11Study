fa76fa5b23dfca173e582397c19b004d2bb64b3c
Serdar <serdar2225@gmail.com>
2021-04-04 23:45:37 -0400

    questions

---
+++ src/main/java/Questions/upend.java
@@ -0,0 +1,13 @@
+package Questions;
+
+public class upend {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(5);
+        sb.append("HOWDY");
+        sb.insert(0, " ");
+        sb.replace(3,5,"LL");
+        sb.insert(6,"COW");
+        sb.delete(2,7);
+        System.out.println(sb.length());
+    }
+}
